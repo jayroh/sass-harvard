4e7b3a1b1733b42abc65a085d044593209e4e739
Joel Oliveira <joel.oliveira@gmail.com>
2012-03-13 19:34:46 -0400

    updating compass

--- config.rb
+++ config.rb
@@ -1,3 +1,4 @@
+require 'susy' 
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -8,13 +9,13 @@ images_dir = "./"
 javascripts_dir = "./"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded # or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
